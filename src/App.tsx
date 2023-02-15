0dc9bc82225f50f530300595e9693c0ab8b295a1
nemanja85 <mitic.nemanja@hotmail.com>
2023-02-15 20:31:26 +0100

    feat: Install tailwind CSS library and made wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { useState } from 'react';
+import words from './wordList.json';
+
 function App() {
+  const [wordToGuest, setWordtoGuest] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="flex items-center flex-col gap-8 m-w-[800px] mx-auto my-0 font-serif font-normal">
+      <div className="text-md text-center font-semibold">Win/Lose</div>
     </div>
   );
 }
