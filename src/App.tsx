2dda491fcedd21567095ad1f6050dfd8843fddb6
nemanja85 <mitic.nemanja@hotmail.com>
2023-09-06 15:10:52 +0200

    feat: Show message win/lose

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,9 @@ function App() {
 
   const inCorrectLetters = guessedLetters.filter((letter) => !wordToGuest.includes(letter));
 
+  const isLoser = inCorrectLetters.length >= 6;
+  const isWinner = wordToGuest.split('').every((letter) => guessedLetters.includes(letter));
+
   const addGuessedLetter = useCallback(
     (letter: string) => {
       if (guessedLetters.includes(letter)) return;
@@ -39,7 +42,10 @@ function App() {
 
   return (
     <div className="flex items-center flex-col gap-8 m-w-[800px] font-serif font-normal py-5 mx-auto my-0">
-      <div className="text-2xl font-semibold text-center">Win/Lose</div>
+      <div className="text-2xl font-semibold text-center">
+        {isLoser && 'Nice try - Refresh page and try again'}
+        {isWinner && 'Congratulations, you are won - Refresh page and try again'}
+      </div>
       <HangmanDrawing numberOfGuesses={inCorrectLetters.length} />
       <HangmanWord guessedLetters={guessedLetters} wordToGuest={wordToGuest} />
       <div style={{ alignSelf: 'stretch' }}>
